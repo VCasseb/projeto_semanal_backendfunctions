ccaf93854504ed4506c851edb9dcf63431233884
Vinicius Casseb <vinicasseb99@gmail.com>
2025-02-27 18:39:25 -0300

    merda

---
+++ httptrigger/__init__.py
@@ -0,0 +1,30 @@
+import logging
+import pyodbc
+import os
+import json
+import uuid
+from datetime import datetime
+import azure.functions as func
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route="httptrigger")
+def httptrigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
